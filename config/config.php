9a6ebf77208d1faf2af5ecbb92100879b749307d
Boldcode <info@boldcode.dev>
2025-11-21 14:00:28 +0100

    work on push to deploy

--- config/config.php
+++ config/config.php
@@ -1,8 +1,5 @@
 <?php
 
 return [
-    'name' => env('MODULE_NAME', 'voids'),
-    'public_path' => env('MODULE_PUBLIC_PATH', '../../public'),
-    'build_path' => env('MODULE_BUILD_PATH', '../../public/build/voids'),
-    'hot_file' => env('MODULE_HOT_FILE', '../../public/hot-voids'),
+    'name' => 'voids',
 ];
